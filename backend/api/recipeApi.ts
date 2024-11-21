1577a952197fcdc70d731792afb7542c11136421
KYinXu <kyleyinxu@gmail.com>
2024-11-20 23:58:41 -0800

    add api call to backend

--- backend/api/recipeApi.ts
+++ backend/api/recipeApi.ts
@@ -1,49 +1,45 @@
-import axios from 'axios';
+var express = require('express');
+var request = require('request');
+var app = express();
 
-const API_BASE_URL = 'http://d1.supercook.com/dyn/results';
 
-export const getRecipes = async (ingredients: string[]) => {
+const API_BASE_URL: string = 'http://d1.supercook.com/dyn/results';
 
-    ingredients = ['butter', 'eggs'];
-    interface requestParams {
-        //needsimage: string,
-        //app: string,
-        kitchen: string[],
-        focus: string,
-        exclude: string,
-        kw: string,
-        catname: string,
-        start: string,
-        fave: string,
-        lang: string,
-        cv: string
 
+app.post('/send-request', (req: any, res: any) => {
+  const headers = {
+    'needsimage': '1',
+    'app': '1',
+    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    "Access-Control-Allow-Methods": "*",
+  }
+  const payload = {
+      'kitchen': ['jalapeno','potato','bell', 'pepper','carrot','egg'],
+      'focus': '',
+      'exclude': '',
+      'kw': '',
+      'catname': '',
+      'start': '0',
+      'fave': 'false',
+      'lang': 'en',
+      'cv': '2'
+  }
+
+
+  request.post({
+    url: API_BASE_URL,
+    json: true,
+    body: payload,
+    headers: headers
+  }, 
+  (error: any, response: any, body: any) => {
+    if (error) {
+      return res.status(500).json({ error: 'Request failed' });
     }
-    const headers = {
-        'needsimage': '1',
-        'app': '1',
-        'Access-Control-Allow-Origin': '*',
-        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-        "Access-Control-Allow-Methods": "*",
-    }
-    const payload: requestParams = {
-        'kitchen': ['jalapeno','potato','bell', 'pepper','carrot','egg'],
-        'focus': '',
-        'exclude': '',
-        'kw': '',
-        'catname': '',
-        'start': '0',
-        'fave': 'false',
-        'lang': 'en',
-        'cv': '2'
-    }
 
-    axios.post(`${API_BASE_URL}`, payload, {headers})
-    .then(response => {
-        console.log(response)
-    })
-    .catch(error => {
-        console.error('Error fetching recipes: ', error)
-    })
-}
-
+    res.json(body);
+    console.log(body);
+    return res.status(200).json(res.json(body));
+  });
+});
