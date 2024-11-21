e4c9d8919a7bfd6fcec2db8d336170833da5560c
KYinXu <kyleyinxu@gmail.com>
2024-11-21 01:17:43 -0800

    test commit

--- services/recipeService.ts
+++ services/recipeService.ts
@@ -5,6 +5,7 @@ const PROXY_URL = 'http://127.0.0.1:5000/proxy';
 export const getRecipes = async (ingredients: string[]) => {
   const url = PROXY_URL;
   //let HEADERS/PARAMS = 
+  //params
   try {
     const response = await axios.post(url);
     console.log(response.data);
@@ -13,4 +14,5 @@ export const getRecipes = async (ingredients: string[]) => {
     console.error('Error fetching data:', error);
     throw error;
   }
+  //test
 };
