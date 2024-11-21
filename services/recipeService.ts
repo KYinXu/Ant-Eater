cd92f76fd8d89cc8529a381381f2308964cd7355
KYinXu <kyleyinxu@gmail.com>
2024-11-21 00:55:39 -0800

    backend

--- services/recipeService.ts
+++ services/recipeService.ts
@@ -5,9 +5,9 @@ const PROXY_URL = 'http://127.0.0.1:5000/proxy';
 export const getRecipes = async (ingredients: string[]) => {
   const url = PROXY_URL;
   //let HEADERS/PARAMS = 
-
   try {
     const response = await axios.post(url);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
