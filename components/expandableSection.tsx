c90441dc577e53f8124fe373a90912ab92775ba1
KYinXu <kyleyinxu@gmail.com>
2024-11-21 23:42:17 -0800

    add rotating arrows

--- components/expandableSection.tsx
+++ components/expandableSection.tsx
@@ -25,8 +25,9 @@ const ExpandableSection: React.FC<ExpandableSectionProps> = ({ title, children }
             </svg>
           ) : (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>
+
           )}
         </div>
         <div className="text-lg font-semibold ml-2">{title}</div>
