d02372a945675bd4394103e5018521631df514d0
kcaisian <147183519+kcaisian@users.noreply.github.com>
2024-11-22 03:01:04 -0800

    page

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-bg1 to-bg2 text-white">
       <header className="flex justify-between items-center p-6">
-        <h1 className="text-5xl font-rokkitt text-white">&Eater</h1>
+        <h1 className="text-5xl font-rokkitt text-white ml-7 mt-2">&Eater</h1>
         <nav>
-          <Link href="/search" className="text-white font-semibold hover:underline -mt-20">
+          <Link href="/search" className="text-white font-semibold hover:underline -mt-20 mr-10 text-xl">
             search
           </Link>
         </nav>
@@ -34,15 +34,18 @@ export default function Home() {
           <div className="">
             <main className="flex flex-col items-center justify-center flex-grow text-center py-20 px-4">
               <div className=' w-4/5'>
-                <img src="https://i.ibb.co/XV5h9NF/Screen-Shot-2024-11-21-at-12-08-30-AM-removebg-preview.png" className="-mt-16 animate-bounce" alt='picture of a banana'></img>
+                <img src="https://i.ibb.co/XV5h9NF/Screen-Shot-2024-11-21-at-12-08-30-AM-removebg-preview.png" className="-mt-8 animate-bounce w-10/12" alt='picture of a banana'></img>
               </div>
             </main>
           </div>
         </div>
       </div>
-      <div className="flex bg-green-500 h-96 mx-24 rounded-3xl backdrop-opacity-60">
+      <div className="flex bg-white h-96 mx-24 rounded-3xl opacity-50">
+        <h2 className="flex font-bebas text-black text-7xl items-center justify-center">
+          Features
+        </h2>
       </div>
-      <footer className="flex justify-center items-center p-6 border-t">
+      <footer className="flex justify-center items-center p-6">
           <p className="text-sm">&copy; 2024 | All rights reserved.</p>
       </footer>
     </div>
